1f9db647564ae7e520d92c869f81dd0148b8d4ea
Fernando Zhapa <ferynando7@gmail.com>
2021-01-18 13:41:53 +0300

    Ranking table

--- biochallenge/apps/challenge/views.py
+++ biochallenge/apps/challenge/views.py
@@ -24,6 +24,41 @@ class ChallengeDetailView(views.DetailView):
     template_name = 'challenge/view.html'
 
 
+class ChallengeSubmissionListView(views.ListView):
+    model = Submission
+    template_name = 'challenge/challenge_submissions.html'
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super(ChallengeSubmissionListView, self).get_queryset(
+            *args, **kwargs)
+        return queryset.filter(release= self.kwargs.get('release_pk'))
+
+# class ChallengeDetailView2(views.DetailView):
+#     #model = Submission
+#     model = Challenge
+#     template_name = 'challenge/view.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super(ChallengeDetailView2, self).get_context_data(**kwargs)
+#         context['submissions'] = Submission.objects.all()
+#         return context
+
+
+    # def get_queryset(self, *args, **kwargs):
+    #     queryset = super(AllSubmissionListView, self).get_queryset(
+    #         *args, **kwargs)
+    #     return queryset# .filter(challenge=self.request.challenge)
+
+
+class ChallengeListView(views.ListView):
+    model = Challenge
+    template_name = 'challenge/list.html'
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super(ChallengeListView, self).get_queryset(
+            *args, **kwargs)
+        return queryset.filter(status=Challenge.ACTIVE)
+
 class SubmissionCreateView(ReleaseMixin, FormRequestMixin, views.CreateView):
     model = Submission
     template_name = 'challenge/submit.html'
